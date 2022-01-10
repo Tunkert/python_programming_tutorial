7bbfff78edacee9518a7c5f725ebb34169f6b6c1
Timothy Unkert <45219053+Tunkert@users.noreply.github.com>
2022-01-09 19:58:56 -0700

    lesson 1 summary

---
+++ lesson_1.py
@@ -0,0 +1,26 @@
+# focus on printing
+
+print("This is how you print a string in python")  # print a string
+print("You can also add two numbers, 2 + 3 =", 2 + 3)  # print a string and an integer sum
+print("My name is " + "Tim")  # concatenate two strings
+name = "Tom"
+print("My name is " + name)  # concatenating a string and a variable that is a string
+sum = 5
+print("2 + 3 is", sum)  # formatting a print statement where you print a string and integer
+# print("2 + 3 is" + sum)  # will throw error
+# you cannot concatenate a string and an int.
+
+f_name = "Sarah"
+print("Her name is " + f_name)
+
+f_name = "John"
+print("His name is " + f_name)  # can reassign variables different values throughout the program
+
+# Variable rules
+# 1. Variables can only contain letters, numbers, and underscores.
+# 2. Variable names can start with a letter or underscore but not with a number.
+# 3. Variable names cannot contain spaces.
+# 4. Variable names cannot contain Python keywords or function names.
+# 5. Variable names should be descriptive but not unnecessarily long.
+# 6. Variables names with lower case l and uppercase O can be confused with 1 and 0.
+ 
